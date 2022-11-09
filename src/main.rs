f7522feb100f0bc3d2d14f74e46a1d4ca71d8be7
Richie Junior <88040624+g1tgudd@users.noreply.github.com>
2022-11-09 17:56:05 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::collections::HashMap;
+use std::io;
+use thiserror::Error;
+
 struct Purchase {
     id: i64,
     name: String,
@@ -20,6 +23,65 @@ impl Purchases {
     fn add(&mut self, purchase: Purchase) {
         self.inner.insert(purchase.id, purchase);
     }
+
+    fn next_id(&self) -> i64 {
+        let mut ids: Vec<_> = self.inner.keys().collect();
+        ids.sort();
+        match ids.pop() {
+            Some(id) => id + 1,
+            None => 1
+        }
+    }
+}
+
+fn get_input() -> Option<String> {
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer).is_err() {
+        println!("Please enter your data again")
+    }
+    let input = buffer.trim().to_owned();
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+
+fn get_price() -> Option<i32> {
+    print!("Price:");
+    loop {
+        let input = match get_input() {
+            Some(input) => input,
+            None => return None
+        };
+        if &input == "" {
+            return None;
+        }
+        let parsed_input: Result<i32, _> = input.parse();
+        match parsed_input {
+            Ok(price) => return Some(price),
+            Err(_) => println!("Please enter the item price")
+        }
+    }
+}
+
+fn get_order (purchases: &mut Purchases) {
+    println!("Bill name: ");
+    let id = match fields.get(0) {
+        Some(id) => i64::from_str_radix(id, 10)?,
+        None => return Err(ParseError::EmptyRecord)
+    };
+    let name = match get_input() {
+        Some(input) => input,
+        None => return
+    };
+    let price: i32  = match get_price() {
+        Some(price) => price,
+        None => return
+    };
+    let purchase = Purchase { id, name, item, quantity, price };
+    purchases.add(purchase);
+    println!("Bill added");
 }
 
 struct History {
