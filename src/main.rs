6defc730b6295a4dbb290512306ffffa3afea89d
Richie Junior <88040624+g1tgudd@users.noreply.github.com>
2022-11-13 22:09:22 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,9 @@
 use std::collections::HashMap;
 use std::io;
-use thiserror::Error;
-
-struct Purchase {
-    id: i64,
-    name: String,
-    item: String,
-    quantity: i32,
-    price: i32,
-}
-
-struct Purchases {
-    inner: HashMap<i64, Purchase>,
-}
-impl Purchases {
-    fn new() -> Self {
-        Self {
-            inner: HashMap::new(),
-        }
-    }
-
-    fn add(&mut self, purchase: Purchase) {
-        self.inner.insert(purchase.id, purchase);
-    }
-
-    fn next_id(&self) -> i64 {
-        let mut ids: Vec<_> = self.inner.keys().collect();
-        ids.sort();
-        match ids.pop() {
-            Some(id) => id + 1,
-            None => 1
-        }
-    }
-}
-
-fn get_input() -> Option<String> {
-    let mut buffer = String::new();
-    while io::stdin().read_line(&mut buffer).is_err() {
-        println!("Please enter your data again")
-    }
-    let input = buffer.trim().to_owned();
-    if &input == "" {
-        None
-    } else {
-        Some(input)
-    }
-}
-
-fn get_price() -> Option<i32> {
-    print!("Price:");
-    loop {
-        let input = match get_input() {
-            Some(input) => input,
-            None => return None
-        };
-        if &input == "" {
-            return None;
-        }
-        let parsed_input: Result<i32, _> = input.parse();
-        match parsed_input {
-            Ok(price) => return Some(price),
-            Err(_) => println!("Please enter the item price")
-        }
-    }
-}
-
-fn get_order (purchases: &mut Purchases) {
-    println!("Customer Name: ");
-    let name = match get_input() {
-        Some(input) => input,
-        None => return
-    };
-    let price: i32  = match get_price() {
-        Some(price) => price,
-        None => return
-    };
-    let purchase = Purchase { id, name, item, quantity, price };
-    purchases.add(purchase);
-    println!("Bill added");
-}
+use std::fs::{File, OpenOptions}; 
+use std::path::PathBuf; 
+use std::error::Error;
+use csv;
 
 struct History {
     id: i64,
@@ -100,6 +25,7 @@ impl Histories {
     }
 }
 
+
 fn main_menu() {
     fn show() {
         println!("");
@@ -111,6 +37,17 @@ fn main_menu() {
         println!("");
         println!("Enter Selection");
     }
+
+    // loop {
+    //     show();
+    //     let input = match get_input() {
+    //         Some(input) => input,
+    //         None => return
+    //     };
+    //     match input.as_str() {
+    //         _ => break
+    //     }
+    // }
 }
 
 fn main() {
